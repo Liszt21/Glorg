85177b6826168efe67d5b55e13ba86fb3adff2ea
Liszt21 <1832666492@qq.com>
2021-10-09 14:17:36 +0800

    modify tags page

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,44 +1,88 @@
-import { graphql, useStaticQuery, Link } from "gatsby"
-import { Tag } from 'antd';
-import Layout from "../layouts"
+import { graphql, useStaticQuery, Link } from "gatsby";
+import { Tooltip, Collapse, Space, Badge } from "antd";
+import Layout from "../layouts";
+import * as React from "react";
+
+const { Panel } = Collapse;
 
 const TagsPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allOrgContent(sort: {order: DESC, fields: metadata___date}) {
+      allOrgContent(sort: { order: DESC, fields: metadata___date }) {
         totalCount
-        nodes  {
+        nodes {
           fields {
             path
           }
           metadata {
             title
             tags
+            summary
           }
         }
       }
     }
-  `)
-  
-  const tags = {}
+  `);
 
-  data.allOrgContent.nodes.forEach(node => {
-    node.metadata.tags.forEach(tag => {
-      if(tags[tag]) {
-        tags[tag].push({title: node.title, path: node.path})
-      }else{
-        tags[tag] = [{title: node.title, path: node.path}]
+  const tags = {};
+
+  data.allOrgContent.nodes.forEach((node) => {
+    node.metadata.tags.forEach((tag) => {
+      if (tags[tag]) {
+        tags[tag].push({
+          title: node.metadata.title,
+          path: node.fields.path,
+          summary: node.metadata.summary
+        });
+      } else {
+        tags[tag] = [
+          {
+            title: node.metadata.title,
+            path: node.fields.path,
+            summary: node.metadata.summary
+          }
+        ];
       }
-    })
-  })
+    });
+  });
 
   return (
     <Layout>
-      <div style={{display: 'flex', flexFlow:"row wrap", justifyContent: 'center', width: "81%", margin: "auto"}}>
-        {Object.keys(tags).map(key => {return (<div style={{cursor:"pointer",margin: ".3rem"}}><Tag>{key}</Tag></div>)})}
-      </div>
+      <Collapse accordion>
+        {Object.keys(tags).map((key) => {
+          const posts = tags[key];
+          return (
+            <Panel
+              header={
+                <Space>
+                  <span>{key}</span>{" "}
+                  <Badge
+                    count={posts.length}
+                    style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+                  />
+                </Space>
+              }
+              key={key}
+            >
+              <Space direction="vertical">
+                {posts.map((post) => {
+                  return (
+                    <Tooltip
+                      placement="right"
+                      title={post.summary}
+                      key={post.title}
+                    >
+                      <Link to={post.path}>{post.title}</Link>
+                    </Tooltip>
+                  );
+                })}
+              </Space>
+            </Panel>
+          );
+        })}
+      </Collapse>
     </Layout>
-  )
-}
+  );
+};
 
 export default TagsPage;
