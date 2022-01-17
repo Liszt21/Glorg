75650e18778bc361aa049e7c10383e7e765ab10d
Liszt21 <1832666492@qq.com>
2022-01-17 15:52:16 +0800

    feat(config): add links.lisp

---
+++ content/links.lisp
@@ -0,0 +1,6 @@
+((:source "~/Notes/Program/Tools/Tmux.org" :target "~/Projects/Glorg/content/snippet/tmux.org")
+ (:source "~/Notes/Program/Tools/OrgMode.org" :target "~/Projects/Glorg/content/snippet/orgmode.org")
+ (:source "~/Notes/Program/Algorithm/Sort.org" :target "~/Projects/Glorg/content/algorithm/sort.org")
+ (:source "~/Notes/Program/Linux/ArchLinux.org" :target "~/Projects/Glorg/content/snippet/archlinux.org")
+ (:source "~/Notes/Program/Linux/Commands.org" :target "~/Projects/Glorg/content/snippet/commands.org")
+ (:source "~/Notes/Program/Lisp/CommonLisp.org" :target "~/Projects/Glorg/content/snippet/commonlisp.org"))
